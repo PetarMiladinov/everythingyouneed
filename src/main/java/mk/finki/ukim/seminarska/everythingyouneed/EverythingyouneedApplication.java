e3eeac1065823b5b988ae4b2adf43884467724b7
Petar Miladinov <miladinovpetar@gmail.com>
2022-03-08 23:45:45 +0100

    Innital Commit

---
+++ src/main/java/mk/finki/ukim/seminarska/everythingyouneed/EverythingyouneedApplication.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.seminarska.everythingyouneed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingyouneedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EverythingyouneedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/finki/ukim/seminarska/everythingyouneed/EverythingyouneedApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.finki.ukim.seminarska.everythingyouneed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingyouneedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
