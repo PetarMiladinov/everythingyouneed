fb3bbe12013b1857dc40674cad22451950797656
Petar Miladinov <miladinovpetar@gmail.com>
2022-04-29 02:34:54 +0200

    WebSecurityConfig, AuthService, master-template, header, footer, login

---
+++ web/src/main/java/mk/finki/ukim/seminarska/everythingyouneed/config/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package mk.finki.ukim.seminarska.everythingyouneed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final CustomUsernamePasswordAuthenticationProvider authenticationProvider;
+
+
+    public WebSecurityConfig(PasswordEncoder passwordEncoder,
+                             CustomUsernamePasswordAuthenticationProvider authenticationProvider) {
+        this.passwordEncoder = passwordEncoder;
+        this.authenticationProvider = authenticationProvider;
+    }
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception{
+
+        httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/register", "/products").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login").permitAll()
+                .failureUrl("/login?error=BadCredentials")
+                .defaultSuccessUrl("/home", true)
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .clearAuthentication(true)
+                .invalidateHttpSession(true)
+                .deleteCookies()
+                .logoutSuccessUrl("/home")
+                .and()
+                .exceptionHandling().accessDeniedPage("/access_denied");
+    }
+}
